fb73241af7b749ff80b262c1dbba3744bbbbaac2
Ceepull <paul.rosario08@gmail.com>
2025-02-10 16:57:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,12 @@
 })();
 */
 const messages = [
-    "Are you sure?",
+    "Sure? D:",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Please...😟",
+    "Iiyak ak😿😿",
+    "Siguro may nag ask na and u said yes☹️☹️☹️",
+    "AKO NA LANGG🤩🤩"
 ];
 
 let messageIndex = 0;
